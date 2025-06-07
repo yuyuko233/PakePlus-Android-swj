4519ad0fa5d635a25dd36d465f97433dbf9ed065
Yuyuko233 <yuyuko233@para.party>
2025-06-07 18:20:50 +0800

    更新 MainActivity.kt

--- app/src/main/java/com/app/pakeplus/MainActivity.kt
+++ app/src/main/java/com/app/pakeplus/MainActivity.kt
@@ -64,7 +64,7 @@ class MainActivity : AppCompatActivity() {
 //        webView.settings.userAgentString =
 //            "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
         webView.settings.loadWithOverviewMode = true
-        webView.settings.setSupportZoom(false)
+        webView.settings.setSupportZoom(true)
 
 
         // clear cache
